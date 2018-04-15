51299ce8f4d56c7c50073e6fdba58791898736e8
ius <bondia@gmail.com>
2018-04-15 11:37:37 +0200

    upgrade react

--- index.ios.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native'
-import { default as TimerApp } from 'app/App'
-
-AppRegistry.registerComponent('ApneaTimer', () => TimerApp)
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import { default as TimerApp } from 'app/App';
+
+AppRegistry.registerComponent('ApneaTimer', () => TimerApp);
