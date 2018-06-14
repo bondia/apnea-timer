079058f842c7a58024e1303e7c0dae06ac06262c
marius.nogueras <marius.nogueras@xailabs.de>
2018-06-14 16:00:47 +0200

    add some dev tools

--- app/App.js
+++ app/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import devTools from 'remote-redux-devtools';
 
 import { initReducers } from './main/redux/reducers';
 import Router from './Router';
 
-const store = createStore(initReducers(), applyMiddleware(thunk));
+const enhancer = compose(
+    applyMiddleware(thunk),
+    devTools({
+        name: Platform.OS,
+        hostname: 'localhost',
+        port: 5678
+    })
+);
+const store = createStore(initReducers(), enhancer);
 
 export default class App extends React.PureComponent {
     render() {
         return (
-            <Provider store={store} >
+            <Provider store={store}>
                 <Router />
             </Provider>
         );
