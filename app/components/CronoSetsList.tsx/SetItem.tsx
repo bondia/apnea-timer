ff58a654ee13f1e3345d3200ccdb69fe3aed1ce5
Marius Nogueras <bondia@gmail.com>
2023-10-19 11:08:44 +0200

    clean up

--- app/components/CronoSetsList.tsx/SetItem.tsx
+++ app/components/CronoSetsList.tsx/SetItem.tsx
@@ -9,30 +9,28 @@ import TextComponent from '../TextComponent/OldTextComponent';
 import * as SC from './SetsList.styled';
 
 // TODO: Migrate to styled components
-const useStyles = (type: SetType, active: boolean) => {
-  return useMemo(() => {
-    const mainColor = type === SetType.SET_TYPE_PREPARE ? COLOR_GREEN_NORMAL : COLOR_RED_NORMAL;
-    return StyleSheet.create({
-      clock: {
-        color: active ? mainColor : FONT_COLOR_GREY,
-        fontSize: 22,
-        lineHeight: 24,
-        textAlign: 'center',
-      },
-      result: {
-        color: !active ? mainColor : FONT_COLOR_GREY,
-        fontSize: 14,
-        lineHeight: 16,
-        textAlign: 'center',
-      },
-      contraction: {
-        color: FONT_COLOR_GREY,
-        fontSize: 14,
-        lineHeight: 16,
-        textAlign: 'center',
-      },
-    });
-  }, [active, type]);
+const getSetItemStyles = (type: SetType, active: boolean) => {
+  const mainColor = type === SetType.SET_TYPE_PREPARE ? COLOR_GREEN_NORMAL : COLOR_RED_NORMAL;
+  return StyleSheet.create({
+    clock: {
+      color: active ? mainColor : FONT_COLOR_GREY,
+      fontSize: 22,
+      lineHeight: 24,
+      textAlign: 'center',
+    },
+    result: {
+      color: !active ? mainColor : FONT_COLOR_GREY,
+      fontSize: 14,
+      lineHeight: 16,
+      textAlign: 'center',
+    },
+    contraction: {
+      color: FONT_COLOR_GREY,
+      fontSize: 14,
+      lineHeight: 16,
+      textAlign: 'center',
+    },
+  });
 };
 
 type Props = {
@@ -56,18 +54,18 @@ const SetItemCrono: FC<Props> = props => {
 
   const isRunning = SetMode.SET_MODE_RUNNING === mode;
 
-  const styles = useStyles(type, active);
+  const styles = getSetItemStyles(type, active);
   const spent = useMemo(() => (started > 0 && ended > 0 ? Math.round((ended - started) / 1000) : 0), [ended, started]);
-  const spentCopy = useMemo(() => secondsToTimeString(spent), [spent]);
-  const durationCopy = useMemo(() => secondsToTimeString(duration), [duration]);
-  const contractionsCopy = useMemo(() => secondsToTimeString(contraction), [contraction]);
+  const spentText = useMemo(() => secondsToTimeString(spent), [spent]);
+  const durationText = useMemo(() => secondsToTimeString(duration), [duration]);
+  const contractionsText = useMemo(() => secondsToTimeString(contraction), [contraction]);
 
   return (
     <SC.GridItem>
       <SC.Set isRunning={isRunning}>
-        <TextComponent style={styles.clock}>{durationCopy}</TextComponent>
-        {spent > 0 && <TextComponent style={styles.result}>{spentCopy}</TextComponent>}
-        {contraction > 0 && <TextComponent style={styles.contraction}>{contractionsCopy}</TextComponent>}
+        <TextComponent style={styles.clock}>{durationText}</TextComponent>
+        {spent > 0 && <TextComponent style={styles.result}>{spentText}</TextComponent>}
+        {contraction > 0 && <TextComponent style={styles.contraction}>{contractionsText}</TextComponent>}
       </SC.Set>
     </SC.GridItem>
   );
