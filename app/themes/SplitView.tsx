cfd902e1a944bab5d0d4987cbb9919a547d2479a
Marius Nogueras <bondia@gmail.com>
2024-01-03 20:18:22 +0100

    more clean up

--- app/themes/SplitView.tsx
+++ app/themes/SplitView.tsx
@@ -2,13 +2,11 @@ import React, { FC, PropsWithChildren } from 'react';
 import { Stack } from '../components/Flow';
 import AppThemeProvider from './AppThemeProvider';
 
-const SplitView: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <Stack grow={1} horizontal>
-      <AppThemeProvider>{children}</AppThemeProvider>
-      <AppThemeProvider isDark>{children}</AppThemeProvider>
-    </Stack>
-  );
-};
+const SplitView: FC<PropsWithChildren> = ({ children }) => (
+  <Stack grow={1} horizontal>
+    <AppThemeProvider>{children}</AppThemeProvider>
+    <AppThemeProvider isDark>{children}</AppThemeProvider>
+  </Stack>
+);
 
 export default SplitView;
