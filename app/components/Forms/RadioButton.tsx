604dd7ab646f85e04e70e17ee5b788281204a048
Marius Nogueras <bondia@gmail.com>
2024-01-04 01:12:38 +0100

    clean up form

---
+++ app/components/Forms/RadioButton.tsx
@@ -0,0 +1,36 @@
+import { Icon } from '@rneui/base';
+import { CheckBox } from '@rneui/themed';
+import React, { FC } from 'react';
+import useAppTheme from '../AppThemeProvider/useAppTheme';
+import Typography from '../Typography/Typography';
+
+type RadioButtonProps = {
+  checked: boolean;
+  title: string;
+  onPress?: () => void;
+};
+
+const RadioButton: FC<RadioButtonProps> = ({ checked, title, onPress }) => {
+  const { colors } = useAppTheme();
+  return (
+    <CheckBox
+      title={<Typography>{title}</Typography>}
+      checked={checked}
+      onPress={onPress}
+      checkedIcon={
+        <Icon
+          name="radio-button-checked"
+          type="material"
+          color={colors.primary900}
+          size={25}
+          iconStyle={{ marginRight: 10 }}
+        />
+      }
+      uncheckedIcon={
+        <Icon name="radio-button-unchecked" type="material" color="grey" size={25} iconStyle={{ marginRight: 10 }} />
+      }
+    />
+  );
+};
+
+export default RadioButton;
--- app/modules/settings/components/ThemeForm.tsx
+++ app/modules/settings/components/ThemeForm.tsx
@@ -1,70 +1,30 @@
-import { Icon } from '@rneui/base';
-import { CheckBox } from '@rneui/themed';
 import React, { FC } from 'react';
 import { useColorScheme } from 'react-native';
-import useAppTheme from '../../../components/AppThemeProvider/useAppTheme';
-import Typography from '../../../components/Typography/Typography';
+import RadioButton from '../../../components/Forms/RadioButton';
 import { ThemeSettingsOptions } from '../../../themes/types';
 import { useSettingThemeContext } from '../context/ThemeSettingsContext';
 
 const ThemeForm: FC = () => {
-  const { colors } = useAppTheme();
   const theme = useColorScheme();
-
   const { theme: storedTheme, updateTheme } = useSettingThemeContext();
-
   return (
     <>
-      <CheckBox
-        title={<Typography>Device Settings ({theme})</Typography>}
+      <RadioButton
+        title={`Device Settings (${theme})`}
         checked={storedTheme === null}
         onPress={() => updateTheme(null)}
-        checkedIcon={
-          <Icon
-            name="radio-button-checked"
-            type="material"
-            color={colors.primary900}
-            size={25}
-            iconStyle={{ marginRight: 10 }}
-          />
-        }
-        uncheckedIcon={
-          <Icon name="radio-button-unchecked" type="material" color="grey" size={25} iconStyle={{ marginRight: 10 }} />
-        }
       />
-      <CheckBox
-        title={<Typography>Light mode</Typography>}
+
+      <RadioButton
+        title="Light mode"
         checked={ThemeSettingsOptions.light === storedTheme}
         onPress={() => updateTheme(ThemeSettingsOptions.light)}
-        checkedIcon={
-          <Icon
-            name="radio-button-checked"
-            type="material"
-            color={colors.primary900}
-            size={25}
-            iconStyle={{ marginRight: 10 }}
-          />
-        }
-        uncheckedIcon={
-          <Icon name="radio-button-unchecked" type="material" color="grey" size={25} iconStyle={{ marginRight: 10 }} />
-        }
       />
-      <CheckBox
-        title={<Typography>Dark side</Typography>}
+
+      <RadioButton
+        title="Dark side"
         checked={ThemeSettingsOptions.dark === storedTheme}
         onPress={() => updateTheme(ThemeSettingsOptions.dark)}
-        checkedIcon={
-          <Icon
-            name="radio-button-checked"
-            type="material"
-            color={colors.primary900}
-            size={25}
-            iconStyle={{ marginRight: 10 }}
-          />
-        }
-        uncheckedIcon={
-          <Icon name="radio-button-unchecked" type="material" color="grey" size={25} iconStyle={{ marginRight: 10 }} />
-        }
       />
     </>
   );
