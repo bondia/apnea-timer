c78050fe2403cdda0fb8b6f2c4fe34518a6df2c2
Marius Nogueras <bondia@gmail.com>
2024-01-16 09:28:31 +0100

    remove unused files

--- app/utils/time/secondsToFullMinutes.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated in favor of milisecondsToTimeString
- */
-const secondsToFullMinutes = (seconds: number): number => {
-  return seconds >= 60 ? Math.floor(seconds / 60) : 0;
-};
-
-export default secondsToFullMinutes;
